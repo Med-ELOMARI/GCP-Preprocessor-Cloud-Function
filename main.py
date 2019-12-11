0fa6c0738bef21c1f11e168170a8c6bdc9027023
Mohamed El Omari <Med-ELOMARI@users.noreply.github.com>
2019-12-11 14:13:14 +0100

    simplified the flow

--- main.py
+++ main.py
@@ -1,77 +1,35 @@
 import os
 import sys
-from datetime import datetime
 
 from flask import jsonify
 
-from Core import (
-    Parser,
-    validate,
-    get_station_config,
-    trilaterator,
-    check_stations_cords,
-)
-from configuration import Config_root, data_root
+from Core import validate, parse, find_location
 
 
 def main(request, test=False):
     try:
         # to be able to test the function locally , added the test parameter
-        if test:
-            data = request
-        else:
-            data = request.get_json()
+
+        data = request if test else request.get_json()
 
         result = validate(data)
 
         if result is "payload":
-            parser = Parser(raw_data=data["data"])
-            DataParsed = parser.parse()
-            data["parsed"] = DataParsed
-            data["timestamp"] = datetime.timestamp(datetime.now())
-            station_pos = [
-                get_station_config(
-                    "station_{}_pos".format(DataParsed["Station_{}_id".format(id)]),
-                    Config_root,
-                )
-                for id in range(1, 4)
-            ]
 
-            check = check_stations_cords(stations=station_pos)
-            if check:
-                data["location"] = check
-            else:
-                trilaterate = trilaterator(
-                    station_1_pos=station_pos[0].get_coordinates(),
-                    rssi_1=DataParsed["Station_1_rssi"],
-                    station_2_pos=station_pos[1].get_coordinates(),
-                    rssi_2=DataParsed["Station_2_rssi"],
-                    station_3_pos=station_pos[2].get_coordinates(),
-                    rssi_3=DataParsed["Station_3_rssi"],
-                )
-                x, y = trilaterate.get_position()
-                data["location"] = {
-                    "x": x,
-                    "y": y,
-                    "ref_{}".format(station_pos[0].station_name): {
-                        "x": station_pos[0].get_coordinates()[0],
-                        "y": station_pos[0].get_coordinates()[1],
-                    },
-                    "ref_{}".format(station_pos[1].station_name): {
-                        "x": station_pos[1].get_coordinates()[0],
-                        "y": station_pos[1].get_coordinates()[1],
-                    },
-                    "ref_{}".format(station_pos[2].station_name): {
-                        "x": station_pos[2].get_coordinates()[0],
-                        "y": station_pos[2].get_coordinates()[1],
-                    },
-                }
+            data = parse(data=data)
+            data = find_location(data=data)
+
+            from configuration import data_root
 
             data_root.document(data["time"]).set(data)
+
             print(data)
+
             return jsonify(data), 200
         elif result is "Config":
             for station, cord in data["Config"].items():
+                from configuration import Config_root
+
                 Config_root.document(station).set(cord)
             data["Status"] = "Config Writen Successfully "
             return jsonify(data), 200
@@ -97,7 +55,9 @@ def main(request, test=False):
 if __name__ == "__main__":
     request = {"time": "Test6", "device": "336B67", "data": "3f0001900002800003752502"}
     try:
-        main(request, test=True)
+        ENV = os.getenv("ENV", "dev")
+        test = True if ENV == "dev" or ENV == "test" else False
+        main(request, test=test)
     except RuntimeError:
         # Flask Context Error , Passed for testing
         pass
